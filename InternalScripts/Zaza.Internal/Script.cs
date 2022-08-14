8d2ccffe6f40b320950c80b4258378ccbe3e3d67
Nexd <statser07@gmail.com>
2022-08-14 11:01:18 +0200

    Internal script

--- InternalScripts/Zaza.Internal/Script.cs
+++ InternalScripts/Zaza.Internal/Script.cs
@@ -8,5 +8,10 @@
         {
             // Beginning of your code
         }
+
+        public override void OnTick()
+        {
+            ZazaConsole.WriteLine($"{this.GetScriptName()} OnTick()");
+        }
     }
 }
