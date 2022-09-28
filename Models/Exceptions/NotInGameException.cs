56cc0fa8b4e976d760055898a82881666e7911c6
Nexd <statser07@gmail.com>
2022-09-28 13:07:03 +0200

    Code style fix

--- Models/Exceptions/NotInGameException.cs
+++ Models/Exceptions/NotInGameException.cs
@@ -5,17 +5,14 @@
     public class NotInGameException : Exception
     {
         public NotInGameException()
-        {
-        }
+            { }
 
         public NotInGameException(string message)
             : base(message)
-        {
-        }
+            { }
 
         public NotInGameException(string message, Exception inner)
             : base(message, inner)
-        {
-        }
+            { }
     }
 }
